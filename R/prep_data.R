4fd731ef775ca7a5f37a0fc04bba3cdf76872054
Alex Hurley <the-Hull@users.noreply.github.com>
2020-11-09 22:13:54 +0100

    - added logic to create dirs and file paths - added above to template script - added logic to modify meta table
    - template script has all steps except datacleanr

--- R/prep_data.R
+++ R/prep_data.R
@@ -1,5 +1,7 @@
 #' Prepare data for assimilation with useful bare-bones script
 #'
+#' Drops last id column for nesting level.
+#'
 #' @param meta_table_path character, csv with some pre-defined headers, including from automated additions from previous runs of this function
 #' @param data_processed_path character, path to `data_processed` folder (i.e. where cleaned data should live)
 #' @param verbose logical, display informative messages and user prompts to proceed?
@@ -11,6 +13,7 @@
 prep_data <- function(meta_table_path,
                       data_processed_path,
                       verbose = TRUE,
+                      overwrite = FALSE,
                       oid = NULL){
 
 
@@ -30,7 +33,8 @@ prep_data <- function(meta_table_path,
     # read file and grab id columns
     meta <- read.csv(file = meta_table_path,
                      header = TRUE,
-                     fileEncoding="UTF-8-BOM")
+                     fileEncoding="UTF-8-BOM",
+                     stringsAsFactors = FALSE)
     colnames(meta) <- tolower(colnames(meta))
 
     id_cols_idx <- grep(pattern = "id[_].*",
@@ -47,7 +51,10 @@ prep_data <- function(meta_table_path,
         meta <- base::transform(meta,
                   prep_script_path	= NA,
                   prep_file_path = NA,
-                  prep_done = NA
+                  prep_done = NA,
+                  clean_path = NA,
+                  clean_done = NA,
+
         )
     }
 
@@ -71,6 +78,11 @@ prep_data <- function(meta_table_path,
     }
 
 
+
+    current_param <- meta$param[start_index]
+
+
+
     # give informative message if verbose
     if(verbose){
 
@@ -83,6 +95,7 @@ prep_data <- function(meta_table_path,
         message("  - parameter parent folder,")
         message("  - ID sub-folders,")
         message("  - prepping script\n")
+        message("And modify the meta table at meta_table_path\n")
 
         user_input <- readline(sprintf("Continue with processing of OID %i? - Y/N:\n", start_index))
         if(user_input %nin% c("Y", "y", "T", "TRUE")){
@@ -93,12 +106,106 @@ prep_data <- function(meta_table_path,
 
 
     # check for and create directory in data_processed_path
+    # make out file_path
 
 
+    if(length(id_cols) > 1){
+        folder_id_cols <- id_cols[-length(id_cols)]
+    }   else {
+        folder_id_cols <- id_cols
+    }
+
+    out_dir_path <- fs::path(data_processed_path,
+                             current_param,
+                             paste0(meta[start_index, folder_id_cols], collapse = "/"))
 
 
+    base_file_name <- paste0(
+        c(current_param,
+          paste0(meta[start_index, id_cols],
+                 collapse = "_")),
+        collapse = "_")
 
-    return(list(id_cols, meta))
+    print(out_dir_path)
+    print(base_file_name)
+    #
+    if(!fs::dir_exists(out_dir_path)){
+        fs::dir_create(out_dir_path, recurse = TRUE)
+        message(sprintf("Creating folder(s): (%s)", out_dir_path))
+    } else {
+        message(sprintf("Output path already exists (%s)", out_dir_path))
+    }
+
+
+    # Make Script template
+    path_prep_script <- fs::path(out_dir_path, base_file_name, ext = "R")
+
+
+    # is file a excel or MS?
+
+    current_file <- meta$file_raw[start_index]
+
+    # manage read in for template
+    if(fs::path_ext(current_file) %in% c("csv", "txt", "dat")){
+        excel <- FALSE
+        text <- TRUE
+    } else if(fs::path_ext(current_file) %in% c("xls", "xlsx")){
+        excel <- TRUE
+        text <- FALSE
+    }
+
+    templating_expr <- expression( usethis::use_template(template = "prep_script.R",
+                                                         save_as = path_prep_script,
+                                                         data = list(param = current_param,
+                                                                     raw_file = current_file,
+                                                                     save_dir = out_dir_path,
+                                                                     base_file_name = base_file_name,
+                                                                     current_index = start_index,
+                                                                     date_time = Sys.Date(),
+                                                                     excel_file = excel,
+                                                                     text_file = text,
+                                                                     meta_table_path = meta_table_path),
+                                                         package = "ROAR",
+                                                         open = TRUE))
+
+
+    # create if doesn't exist
+    # if exists, don't do anything - unless in verbose mode, where choice is given.
+    if(!fs::file_exists(path_prep_script) | overwrite){
+
+        if(overwrite){
+            message("Prepping script already exists. Overwriting due to overwrite = TRUE")
+        }
+
+        eval(templating_expr)
+
+    } else {
+
+        if(verbose){
+            user_input <- readline(sprintf("Processing script for this OID (%i?) exists. Overwrite? - Y/N:\n", start_index))
+            if(user_input %nin% c("Y", "y", "T", "TRUE")){
+                stop("Aborted processing.")
+            } else {
+
+                eval(templating_expr)
+            }
+
+        } else {
+            stop(sprintf("Processing script for this OID (= %i) exists. Aborting", start_index))
+
+        }
+
+    }
+
+
+    meta[start_index, "prep_script_path"] <- path_prep_script
+
+    write.csv(x = meta,
+              file = meta_table_path,
+              row.names = FALSE)
+
+
+    # return(list(id_cols, meta))
 
 
 
---
+++ inst/templates/prep_script.R
@@ -0,0 +1,104 @@
+# META -------------------------------------------------------------------
+# ROAR for
+# parameter: {{param}}
+# sample_file_id: {{base_file_name}}
+# Created on {{date_time}}
+# Author:
+# START ------------------------------------------------------------------------
+
+
+
+
+# Read data ---------------------------------------------------------------
+
+
+## parameters for reading
+
+
+skip <- 0
+{{#excel_file}}
+
+{{base_file_name}} <- janitor::clean_names(readxl::read_excel(path = "{{raw_file}}",
+                                         skip = skip))
+{{/excel_file}}
+{{#text_file}}
+header <- TRUE
+stringsAsFactors <- FALSE
+
+{{base_file_name}} <- janitor::clean_names(utils::read_csv(path = "{{raw_file}}",
+                                             skip = skip,
+                                      header = header,
+                                      stringsAsFactors = stringsAsFactors))
+
+{{/text_file}}
+
+## add additional cleaning steps, where necessary
+
+# Reshape to long format --------------------------------------------------
+
+# this section will always need manual adjustments
+# modify / add as necessary
+
+identifier_columns <- c("timestamp")
+# identifier_columns <- c("timestamp", "sample")
+# identifier_columns <- c("timestamp", "sample_id")
+
+{{base_file_name}} <- tidyr::pivot_longer(data = {{base_file_name}},
+                                          cols = !identifier_columns,
+                                          names_to = "sensor",
+                                          values_to = "value")
+
+
+# Save PREP data ---------------------------------------------------------------
+
+# save to {{save_dir}}
+
+meta <- read.csv(file = "{{meta_table_path}}",
+                 header = TRUE,
+                 fileEncoding="UTF-8-BOM",
+                 stringsAsFactors = FALSE)
+colnames(meta) <- tolower(colnames(meta))
+
+path_save_prep <- fs::path("{{save_dir}}",
+                           paste("{{base_file_name}}", "prep", sep = "_"),
+                                ext = "RDS")
+
+
+meta[{{current_index}}, "prep_file_path"] <- path_save_prep
+
+write.csv(x = meta,
+          file = "{{meta_table_path}}",
+          row.names = FALSE)
+
+
+
+# Update meta table (PREP) -------------------------------------------------------
+
+meta <- read.csv(file = meta_table_path,
+                 header = TRUE,
+                 fileEncoding="UTF-8-BOM",
+                 stringsAsFactors = FALSE)
+colnames(meta) <- tolower(colnames(meta))
+
+meta[{{current_index}}, "prep_done"] <- TRUE
+
+
+# Run datacleanr (CLEAN)----------------------------------------------------------
+
+
+
+
+# copy CLEAN to data_processed -----------------------------------
+
+
+
+# Update meta table (CLEAN) -------------------------------------------------------
+
+meta <- read.csv(file = meta_table_path,
+                 header = TRUE,
+                 fileEncoding="UTF-8-BOM",
+                 stringsAsFactors = FALSE)
+colnames(meta) <- tolower(colnames(meta))
+
+meta[{{current_index}}, "clean_done"] <- TRUE
+
